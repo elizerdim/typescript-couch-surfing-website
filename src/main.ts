3b0388f794b6e691b9460d119fd32b252830e07d
Eliz Erdim <elizerdim@gmail.com>
2024-05-04 00:36:25 +0300

    Add reviews data

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
+import { Review } from './interfaces'
+import { LoyaltyUser } from './enums'
 import './style.css'
 
+// Reviews
+const reviews: Review[] = [
+  {
+      name: 'Sheila',
+      stars: 5,
+      loyaltyUser: LoyaltyUser.GOLD_USER,
+      date: '01-04-2021'
+  },
+  {
+      name: 'Andrzej',
+      stars: 3,
+      loyaltyUser: LoyaltyUser.BRONZE_USER,
+      date: '28-03-2021'
+  },
+  {
+      name: 'Omar',
+      stars: 4,
+      loyaltyUser: LoyaltyUser.SILVER_USER,
+      date: '27-03-2021',
+  },
+]
